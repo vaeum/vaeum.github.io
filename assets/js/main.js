a94a282041e5edab0446bc535d34bb64ab120ab3
Alexander Anpleenko <vaeum@yahoo.com>
2016-09-05 00:27:26 +0300

    add linkedin

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@ document.getElementById('footer-date').innerHTML = new Date().getFullYear();
 
 
 var Share = {
-  vkontakte: function(purl, ptitle, pimg, text) {
+  vk: function(purl, ptitle, pimg, text) {
     url  = 'http://vkontakte.ru/share.php?';
     url += 'url='          + encodeURIComponent(purl);
     url += '&title='       + encodeURIComponent(ptitle);
@@ -46,13 +46,13 @@ var Share = {
     url += '&noparse=true';
     Share.popup(url);
   },
-  odnoklassniki: function(purl, text) {
+  ok: function(purl, text) {
     url  = 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1';
     url += '&st.comments=' + encodeURIComponent(text);
     url += '&st._surl='    + encodeURIComponent(purl);
     Share.popup(url);
   },
-  facebook: function(purl, ptitle, pimg, text) {
+  fb: function(purl, ptitle, pimg, text) {
     url  = 'http://www.facebook.com/sharer.php?s=100';
     url += '&p[title]='     + encodeURIComponent(ptitle);
     url += '&p[summary]='   + encodeURIComponent(text);
@@ -60,7 +60,7 @@ var Share = {
     url += '&p[images][0]=' + encodeURIComponent(pimg);
     Share.popup(url);
   },
-  twitter: function(purl, ptitle) {
+  tw: function(purl, ptitle) {
     url  = 'http://twitter.com/share?';
     url += 'text='      + encodeURIComponent(ptitle);
     url += '&url='      + encodeURIComponent(purl);
@@ -73,6 +73,13 @@ var Share = {
     url += '&title='       + encodeURIComponent(ptitle);
     url += '&description=' + encodeURIComponent(text);
     url += '&imageurl='    + encodeURIComponent(pimg);
+    Share.popup(url)
+  },
+  li: function(purl, ptitle, psummary) {
+    url =  'http://www.linkedin.com/shareArticle?mini=true';
+    url += '&url=' + encodeURIComponent(purl);
+    url += '&title=' + encodeURIComponent(ptitle);
+    url += '&summary=' + encodeURIComponent(psummary);
     Share.popup(url)
   },
 
